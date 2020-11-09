48df76509ce7efb1c2500b2cbbc3e9b5190f4e47
hd-rk <hdn314159@gmail.com>
2020-11-09 17:41:37 +0800

    fix: null default

--- gcp/gke.tf
+++ gcp/gke.tf
@@ -1,13 +1,10 @@
 locals {
   gke_preset_conf = local.preset_map[var.preset]["gke"]
-  gke_custom_conf = {
-    regional = var.gke_regional
-    max_pods_per_node = var.gke_max_pods_per_node
-    pod_secondary_range_name = var.gke_pod_secondary_range_name
-    svc_secondary_range_name = var.gke_svc_secondary_range_name
-    node_pools = var.gke_node_pools
-  }
-  gke_merged_conf = merge(local.gke_preset_conf, local.gke_custom_conf)
+  gke_regional = var.gke_regional != null ? var.gke_regional : gke_preset_conf["regional"]
+  gke_max_pods_per_node = var.gke_max_pods_per_node != null ? var.gke_max_pods_per_node : gke_preset_conf["max_pods_per_node"]
+  gke_pod_secondary_range_name = var.gke_pod_secondary_range_name != null ? var.gke_pod_secondary_range_name : gke_preset_conf["pod_secondary_range_name"]
+  gke_svc_secondary_range_name = var.gke_svc_secondary_range_name != null ? var.gke_svc_secondary_range_name : gke_preset_conf["svc_secondary_range_name"]
+  gke_node_pools = var.gke_node_pools != null ? var.gke_node_pools : gke_preset_conf["node_pools"]
 }
 
 module "gke" {
@@ -21,12 +18,12 @@ module "gke" {
   subnet = var.subnet
   enable_monitoring = local.enable_monitoring
 
-  regional = local.gke_merged_conf["regional"]
+  regional = local.gke_regional
   private = var.gke_private
-  max_pods_per_node = local.gke_merged_conf["max_pods_per_node"]
-  pod_secondary_range_name = local.gke_merged_conf["pod_secondary_range_name"]
-  svc_secondary_range_name = local.gke_merged_conf["svc_secondary_range_name"]
-  node_pools = local.gke_merged_conf["node_pools"]
+  max_pods_per_node = local.gke_max_pods_per_node
+  pod_secondary_range_name = local.gke_pod_secondary_range_name
+  svc_secondary_range_name = local.gke_svc_secondary_range_name
+  node_pools = local.gke_node_pools
   master_auth_networks = var.gke_master_auth_networks
   oauth_scopes = var.gke_oauth_scopes
   node_management = var.gke_node_management
